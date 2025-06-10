f62b3e82f0431567da20cc436e1654682a71e581
mrjleo <mrjleo@users.noreply.github.com>
2025-06-10 17:19:58 +0200

    add db module and schema

---
+++ db/schema.py
@@ -0,0 +1,42 @@
+from peewee import CharField, ForeignKeyField, Model, SqliteDatabase
+
+DATABASE = SqliteDatabase(None)
+DATABASE.init("data.db")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = DATABASE
+
+
+class Query(BaseModel):
+    q_id = CharField(primary_key=True)
+    dataset_name = CharField()
+    text = CharField()
+
+
+class Intent(BaseModel):
+    i_id = CharField()
+    query = ForeignKeyField(Query, backref="intents")
+    text = CharField()
+
+
+class Document(BaseModel):
+    d_id = CharField(primary_key=True)
+    corpus_name = CharField()
+    text = CharField()
+
+
+class Triple(BaseModel):
+    query = ForeignKeyField(Query, backref="triples")
+    intent = ForeignKeyField(Intent, backref="triples", null=True)
+    document = ForeignKeyField(Document, backref="triples")
+
+
+class LLM(BaseModel):
+    name = CharField()
+
+
+class Annotation(BaseModel):
+    triple = ForeignKeyField(Triple, backref="annotations")
+    llm = ForeignKeyField(LLM, backref="annotations")
