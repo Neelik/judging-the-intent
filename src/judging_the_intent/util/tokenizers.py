faf8766fb65771a4b0fd48f946eeeed785b23f82
Garrett Allen <neelik.n33l@gmail.com>
2025-09-11 16:01:15 +0200

    Fixed typos in lookup for HF model names.

--- src/judging_the_intent/util/tokenizers.py
+++ src/judging_the_intent/util/tokenizers.py
@@ -1,5 +1,5 @@
 def tokenizer_lookup(model_id):
     tokenizers = {
-        "mistral:7b-instruct-v0.3-q4 0": "mistralai/Mistral-7B-Instruct-v0.3",
-        "llama3.1:8b-instruct-q4 K M": "meta-llama/Meta-Llama-3.1-8B-Instruct"
+        "mistral:7b-instruct-v0.3-q4_0": "mistralai/Mistral-7B-Instruct-v0.3",
+        "llama3.1:8b-instruct-q4_K_M": "meta-llama/Meta-Llama-3.1-8B-Instruct"
     }
