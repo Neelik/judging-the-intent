b5b260310dca2e609069f9da7b324b972dc098f6
Garrett Allen <neelik.n33l@gmail.com>
2025-09-11 16:07:02 +0200

    Added missing return statement to tokenizer name lookup.

--- src/judging_the_intent/util/tokenizers.py
+++ src/judging_the_intent/util/tokenizers.py
@@ -3,3 +3,5 @@ def tokenizer_lookup(model_id):
         "mistral:7b-instruct-v0.3-q4_0": "mistralai/Mistral-7B-Instruct-v0.3",
         "llama3.1:8b-instruct-q4_K_M": "meta-llama/Meta-Llama-3.1-8B-Instruct"
     }
+
+    return tokenizers[model_id]
