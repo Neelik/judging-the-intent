d63a45c5b6b7f91ae30f7361d0208d95ef477bc0
Garrett Allen <neelik.n33l@gmail.com>
2025-09-22 10:59:41 +0200

    Fixed error in HF name for Llama3.2

--- src/judging_the_intent/util/tokenizers.py
+++ src/judging_the_intent/util/tokenizers.py
@@ -2,7 +2,7 @@ def tokenizer_lookup(model_id):
     tokenizers = {
         "mistral:7b-instruct-v0.3-q4_0": "mistralai/Mistral-7B-Instruct-v0.3",
         "llama3.1:8b-instruct-q4_K_M": "meta-llama/Meta-Llama-3.1-8B-Instruct",
-        "llama3.2:3b-instruct-q4_K_M": "meta-llama/Meta-Llama-3.2-3B-Instruct",
+        "llama3.2:3b-instruct-q4_K_M": "meta-llama/Llama-3.2-3B-Instruct",
     }
 
     return tokenizers[model_id]
