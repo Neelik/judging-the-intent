5c0c047db876fb0e31726b463c3bac208427ff89
Garrett Allen <neelik.n33l@gmail.com>
2025-09-30 14:32:38 +0200

    Refactored judgment inspection to reperform annotaiton on subset of items.

--- src/judging_the_intent/util/inspect_judgment.py
+++ src/judging_the_intent/util/inspect_judgment.py
@@ -5,6 +5,7 @@ import sys
 import json
 import requests
 from pathlib import Path
+from tqdm import tqdm
 
 LOGGER = logging.getLogger(__file__)
 
@@ -21,7 +22,8 @@ from judging_the_intent.db.schema import (
 
 OLLAMA_API = f"{os.environ.get('OLLAMA_HOST', 'http://localhost:11434')}/api"
 
-def main(doc_id, query_id, intent_id, model):
+# def main(doc_id, query_id, intent_id, model):
+def main(dataset, model):
     logging.basicConfig(level=logging.INFO)
 
     config = Config.get(
@@ -33,76 +35,110 @@ def main(doc_id, query_id, intent_id, model):
         LOGGER.error("could not find model %s", model)
         sys.exit(1)
 
-    doc = Document.select().where(Document.d_id == doc_id)
-    intent = Intent.select().where(Intent.i_id == intent_id)
-    query = Query.select().where(Query.q_id == query_id)
-    triple = Triple.select().where(Triple.intent.in_(intent)).where(Triple.query.in_(query)).where(Triple.document.in_(doc))
-    annotation = Annotation.select().where(Annotation.triple.in_(triple))
-
-    query = query.get_or_none()
-    intent = intent.get_or_none()
-    doc = doc.get_or_none()
-    annotation = annotation.get_or_none()
     data_dir = Path(__file__).parent.parent.parent.parent.joinpath(
-        "trec-web", "qrels", f"{query.dataset_name.replace('/', '-')}-filtered-qrels.tsv")
+        "trec-web", "qrels", f"{dataset.replace('/', '-')}-filtered-qrels.tsv")
     dataset_df = pd.read_csv(data_dir, sep="\t", names=["query_id", "intent_id", "doc_id", "rel"])
-    human_annotation = dataset_df[(dataset_df["query_id"] == query_id) & (dataset_df["intent_id"] == intent_id) & (dataset_df["doc_id"] == doc_id)]
+    dataset_df = dataset_df[dataset_df["rel"] >= 2]
+    intent_reannotations = []
+    without_intent_reannotations = []
 
-    data = {
-        "prompt": build_prompt(
-            query.text, intent.text, doc.text, version="verbose"
-        ),
-        "model": model,
-        "stream": False,
-    }
+    LOGGER.info(f"Found {dataset_df.shape[0]} documents with positive relevance scores.")
+    for entry in tqdm(dataset_df.itertuples(index=False), total=dataset_df.shape[0]):
+        doc = Document.select().where(Document.d_id == entry.doc_id)
+        intent = Intent.select().where(Intent.i_id == entry.intent_id)
+        query = Query.select().where(Query.q_id == entry.query_id)
+        triple = Triple.select().where(Triple.intent.in_(intent)).where(Triple.query.in_(query)).where(Triple.document.in_(doc))
+        annotation = Annotation.select().where(Annotation.triple.in_(triple))
 
-    LOGGER.info(" >> Seeking Explanation with Intent")
-    try:
-        api_response = requests.post(
-            url=f"{OLLAMA_API}/generate",
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
-        result = json.loads(api_response.text)["response"]
-        print(f"NEW:\t{json.loads(result)['Relevance Score']}\tOLD:\t{annotation.result}\tHUMAN:\t{human_annotation.rel.values[0]}\n\n")
-        print(f"{json.loads(result)['Explanation']}")
-    except Exception as e:
-        print("Annotation failed.")
-        error = repr(e)
-        print(error)
+        query = query.get_or_none()
+        intent = intent.get_or_none()
+        doc = doc.get_or_none()
+        annotation = annotation.get_or_none()
+        # human_annotation = dataset_df[(dataset_df["query_id"] == entry.query_id) & (dataset_df["intent_id"] == entry.intent_id) & (dataset_df["doc_id"] == entry.doc_id)]
+        data = {
+            "prompt": build_prompt(
+                query.text, intent.text, doc.text, version="verbose"
+            ),
+            "model": model,
+            "stream": False,
+        }
 
-    LOGGER.info(" >> Seeking Explanation without Intent")
-    data["prompt"] = build_prompt(query.text, None, doc.text, version="verbose")
-    try:
-        api_response = requests.post(
-            url=f"{OLLAMA_API}/generate",
-            data=json.dumps(data),
-            headers={"Content-Type": "application/json"},
-        )
-        result = json.loads(api_response.text)["response"]
-        print(f"NEW:\t{json.loads(result)['Relevance Score']}\tOLD:\t{annotation.result}\tHUMAN:\t{human_annotation.rel.values[0]}\n\n")
-        print(f"{json.loads(result)['Explanation']}")
-    except Exception as e:
-        print("Annotation failed.")
-        error = repr(e)
-        print(error)
+        LOGGER.info(" >> Seeking Explanation with Intent")
+        try:
+            api_response = requests.post(
+                url=f"{OLLAMA_API}/generate",
+                data=json.dumps(data),
+                headers={"Content-Type": "application/json"},
+            )
+            result = json.loads(api_response.text)["response"]
+            # print(f"NEW:\t{json.loads(result)['Relevance Score']}\tOLD:\t{annotation.result}\tHUMAN:\t{human_annotation.rel.values[0]}\n\n")
+            # print(f"{json.loads(result)['Explanation']}")
+            intent_reannotations.append({
+                "query": query.text,
+                "intent": intent.text,
+                "doc": doc.text,
+                "annotation": json.loads(result)['Relevance Score'],
+                "explanation": json.loads(result)['Explanation'],
+                "human_judgment": entry.rel,
+                "old_llm_judgment": annotation.result,
+            })
+        except Exception as e:
+            print("Annotation failed.")
+            error = repr(e)
+            print(error)
 
+        LOGGER.info(" >> Seeking Explanation without Intent")
+        data["prompt"] = build_prompt(query.text, None, doc.text, version="verbose")
+        try:
+            api_response = requests.post(
+                url=f"{OLLAMA_API}/generate",
+                data=json.dumps(data),
+                headers={"Content-Type": "application/json"},
+            )
+            result = json.loads(api_response.text)["response"]
+            # print(f"NEW:\t{json.loads(result)['Relevance Score']}\tOLD:\t{annotation.result}\tHUMAN:\t{human_annotation.rel.values[0]}\n\n")
+            # print(f"{json.loads(result)['Explanation']}")
+            without_intent_reannotations.append({
+                "query": query.text,
+                "intent": intent.text,
+                "doc": doc.text,
+                "annotation": json.loads(result)['Relevance Score'],
+                "explanation": json.loads(result)['Explanation'],
+                "human_judgment": entry.rel,
+                "old_llm_judgment": annotation.result,
+            })
+        except Exception as e:
+            print("Annotation failed.")
+            error = repr(e)
+            print(error)
+
+    intent_reannotations_df = pd.DataFrame(intent_reannotations)
+    without_intent_reannotations_df = pd.DataFrame(without_intent_reannotations)
+
+    intent_reannotations_df.to_csv(
+        Path(__file__).parent.parent.parent.parent.joinpath("trec-web", f"inspection-intent-{dataset.replace('/', '-')}.csv"),
+        index=False)
+    without_intent_reannotations_df.to_csv(
+        Path(__file__).parent.parent.parent.parent.joinpath("trec-web", f"inspection-no-intent-{dataset.replace('/', '-')}.csv"),
+        index=False)
     # print(f"DOCUMENT\n{doc.text}\n\nQUERY\n{query.text}\n\nINTENT\n{intent.text}\n\nLLM Judgement: {annotation.result}\t\tHuman Judgement: {human_annotation.rel.values[0]}\n\n")
 
 
 if __name__ == "__main__":
     running = True
     model = input("Please enter the Ollama model identifier: ")
-    while running:
-        doc_id = input("Enter document ID: ")
-        query_id = int(input("Enter query ID: "))
-        intent_id = int(input("Enter intent ID: "))
+    dataset = input("Please enter the dataset identifier: ")
+    main(dataset, model)
+    # while running:
+        # doc_id = input("Enter document ID: ")
+        # query_id = int(input("Enter query ID: "))
+        # intent_id = int(input("Enter intent ID: "))
 
-        try:
-            main(doc_id, query_id, intent_id, model)
-        except Exception:
-            pass
-
-        cont = input("Continue? (y/n): ")
-        if cont == "n":
-            running = False
+        # try:
+        #     main(doc_id, query_id, intent_id, model)
+        # except Exception:
+        #     pass
+        #
+        # cont = input("Continue? (y/n): ")
+        # if cont == "n":
+        #     running = False
